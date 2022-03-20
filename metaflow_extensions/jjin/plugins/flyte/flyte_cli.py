f681ff50d0b2b372262ed18579a3b9e8abb31a83
Jonathan Jin <me@jonathanj.in>
2022-03-20 16:52:17 -0400

    Start logic for converting Metaflow flow to Flyte workflow

--- metaflow_extensions/jjin/plugins/flyte/flyte_cli.py
+++ metaflow_extensions/jjin/plugins/flyte/flyte_cli.py
@@ -1,6 +1,8 @@
 import click
 from metaflow import current
 
+from metaflow_extensions.jjin.plugins.flyte.workflow import WorkflowConstructor
+
 
 # Provides new CLI flow subcmds
 @click.group()
@@ -18,3 +20,13 @@ def flyte():
 def register(ctx):
     ctx.obj.echo(f"Project name: {current.project_name}")
     ctx.obj.echo(f"Branch name: {current.branch_name}")
+
+
+@flyte.command(help="Compile to Flyte workflow.")
+@click.pass_obj
+def compile(obj):
+    ctor = WorkflowConstructor.from_metaflow_cli_obj(obj)
+
+    obj.echo(f"Project name: {current.project_name}")
+    obj.echo(f"Branch name: {current.branch_name}")
+    wf = ctor.build(obj.graph)
---
+++ metaflow_extensions/jjin/plugins/flyte/workflow.py
@@ -0,0 +1,34 @@
+from typing import Callable, Optional
+
+from flytekit import Workflow
+from metaflow import current
+from metaflow.graph import FlowGraph
+
+# TODO: func to turn node into flyte.PythonTask
+
+
+class WorkflowConstructor:
+    def __init__(self, echo: Callable = print):
+        self.echo: Callable = echo
+
+    @classmethod
+    def from_metaflow_cli_obj(cls, obj) -> "WorkflowConstructor":
+        return cls(echo=obj.echo)
+
+    def build(self, flow_graph: FlowGraph) -> Workflow:
+        self.echo(str(flow_graph))
+
+        # Provided by @project decorator if present
+        #
+        # TODO: Require this?
+        project: Optional[str] = current.get("project_name")
+        domain: Optional[str] = current.get("branch_name")
+
+        wf = Workflow(
+            name=flow_graph.name,
+        )
+
+        self.echo("Ignoring all nodes in the workflow; stuff's WIP okay?")
+        self.echo(f"Constructed shell Flyte workflow: {wf}")
+
+        return wf
