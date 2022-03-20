0b5118239d43a2c078ddf4ca45f33e3093d4fedd
Jonathan Jin <me@jonathanj.in>
2022-03-20 13:41:36 -0400

    Start creating some experimental flows

---
+++ flows/00-skeleton.py
@@ -0,0 +1,22 @@
+from metaflow import FlowSpec, step
+
+
+class SkeletonFlow(FlowSpec):
+    """A flow that contains literally only a `start` and an `end`. It has no logic,
+    no external dependencies, no resource requirements -- nothing.
+
+    """
+    @step
+    def start(self):
+        """Prints a message stating that the flow is starting. That's it."""
+        print("SkeletonFlow is starting.")
+        self.next(self.end)
+
+    @step
+    def end(self):
+        """Prints a message stating that the flow is ending. That's it."""
+        print("SkeletonFlow is ending.")
+
+
+if __name__ == "__main__":
+    SkeletonFlow()
