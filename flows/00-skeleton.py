aa5ad1756e2a7f9bcde96924101a4958681712bc
Jonathan Jin <me@jonathanj.in>
2022-03-20 23:27:59 -0400

    Refining code

--- flows/00-skeleton.py
+++ flows/00-skeleton.py
@@ -1,11 +1,7 @@
 from metaflow import FlowSpec, project, step
 
-# from metaflow import flyte
 
-
-# FIXME: With flyte plugin CLI, specific decorator might not even be necessary.
-# @flyte
-@project(name="jjin_metaflow_flyte_project")
+@project(name="flytesnacks")
 class SkeletonFlow(FlowSpec):
     """A flow that contains literally only a `start` and an `end`. It has no logic,
     no external dependencies, no resource requirements -- nothing.
--- metaflow_extensions/jjin/plugins/flyte/flyte_cli.py
+++ metaflow_extensions/jjin/plugins/flyte/flyte_cli.py
@@ -1,8 +1,14 @@
+from typing import Optional
+
 import click
+from flytekit import LaunchPlan
+from flytekit.core.context_manager import get_image_config
+from flytekit.remote.remote import FlyteRemote
 from metaflow import current
+from metaflow.exception import (FlyteMissingProjectAndBranchException,
+                                FlyteNotReadyException)
 
 from metaflow_extensions.jjin.plugins.flyte.workflow import WorkflowConstructor
-from flytekit import LaunchPlan
 
 
 # Provides new CLI flow subcmds
@@ -12,31 +18,52 @@ def cli():
 
 
 @cli.group(help="Commands related to Flyte.")
-def flyte():
+@click.pass_obj
+def flyte(obj):
     # TODO: Initialize flytekit.remote.remote.FlyteRemote; set to obj.flyte_cluster
     #
     # https://docs.flyte.org/projects/flytekit/en/latest/generated/flytekit.remote.remote.FlyteRemote.html
-    pass
+
+    project_name: Optional[str] = current.get("project_name")
+    branch_name: Optional[str] = current.get("branch_name")
+
+    if not project_name or not branch_name:
+        raise FlyteMissingProjectAndBranchException
+
+    obj.flyte_cluster = FlyteRemote(
+        "localhost:30081",
+        insecure=True,
+        default_project=project_name,
+        default_domain=branch_name,
+        image_config=get_image_config(img_name="myapp:v1"),
+    )
+
+    # TODO: Create the project if not present??
+    #
+    # obj.flyte_cluster.client.register_project
+
+    obj.workflow_constructor = WorkflowConstructor.from_metaflow_cli_obj(obj)
+    obj.workflow = obj.workflow_constructor.build(obj.graph)
 
 
 @flyte.command(help="Register flow as a Flyte workflow.")
-@click.pass_context
-def register(ctx):
-    ctx.obj.echo(f"Project name: {current.project_name}")
-    ctx.obj.echo(f"Branch name: {current.branch_name}")
+@click.pass_obj
+def register(obj):
+    obj.echo(f"Project name: {current.project_name}")
+    obj.echo(f"Branch name: {current.branch_name}")
+    obj.flyte_cluster.register(obj.workflow)
 
 
 @flyte.command(help="Compile to Flyte workflow.")
 @click.pass_obj
 def compile(obj):
-    print(obj.graph.nodes["start"])
-    ctor = WorkflowConstructor.from_metaflow_cli_obj(obj)
-
     obj.echo(f"Project name: {current.project_name}")
     obj.echo(f"Branch name: {current.branch_name}")
-    wf = ctor.build(obj.graph)
-
+    wf = obj.workflow
     obj.echo(f"Ready? {wf.ready()}")
+
+    if not wf.ready():
+        raise FlyteNotReadyException
 
     launch_plan: LaunchPlan = LaunchPlan.get_or_create(wf)
 
